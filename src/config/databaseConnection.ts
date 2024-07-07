806737e265ad9ddb721d724a716512e8c0ff8512
Pedro <pedrohlucena413@gmail.com>
2024-07-07 17:35:32 -0300

    feat: add database connection

--- src/config/databaseConnection.ts
+++ src/config/databaseConnection.ts
@@ -1,8 +1,8 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 export const sequelize = new Sequelize(process.env.POSTGRES_DATABASE_URL, {
   dialect: 'postgres',
-});
+})
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,27 @@
-import express from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import { GraphQLSchema } from 'graphql';
-import { sequelize } from './config';
-import cors from 'cors';
+import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import { GraphQLSchema } from 'graphql'
+import { sequelize } from './config'
+import cors from 'cors'
 
+const schema = {} as GraphQLSchema
 
-const schema = {} as GraphQLSchema;
+;(async () => {
+  const app = express()
+  const port = process.env.PORT || 4000
 
-(
-    async () => {
-        const app = express();
-        const port = process.env.PORT || 4000;
+  app.use(cors())
+  app.use(
+    '/graphql',
+    graphqlHTTP({
+      schema,
+      graphiql: true,
+      pretty: true,
+    }),
+  )
 
-        app.use(cors());
-        app.use(
-            '/graphql', 
-            graphqlHTTP(
-                {
-                    schema,
-                    graphiql: true,
-                    pretty: true
-                }
-            )
-        );
+  await sequelize.sync()
 
-        await sequelize.sync()
-
-        const okMessage = `Server is running on http://localhost:${port}/graphql`
-        app.listen(port, () => console.log(okMessage));        
-    }
-)()
+  const okMessage = `Server is running on http://localhost:${port}/graphql`
+  app.listen(port, () => console.log(okMessage))
+})()
