271997ce8c366e27914654dba922fdf0360548ec
Pedro <pedrohlucena413@gmail.com>
2024-07-07 16:10:10 -0300

    feat: add database connection

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
 import express from 'express';
-import cors from 'cors';
 import { graphqlHTTP } from 'express-graphql';
 import { GraphQLSchema } from 'graphql';
+import { sequelize } from './config';
+import cors from 'cors';
 
-const app = express();
 
 const schema = {} as GraphQLSchema;
 
-app.use(cors());
-app.use(
-    '/graphql', 
-    graphqlHTTP(
-        {
-            schema,
-            graphiql: true,
-            pretty: true
-        }
-    )
-);
+(
+    async () => {
+        const app = express();
+        const port = process.env.PORT || 4000;
 
-app.listen(4000)
+        app.use(cors());
+        app.use(
+            '/graphql', 
+            graphqlHTTP(
+                {
+                    schema,
+                    graphiql: true,
+                    pretty: true
+                }
+            )
+        );
 
-console.log('SERVER OK!');
+        await sequelize.sync()
+
+        const okMessage = `Server is running on http://localhost:${port}/graphql`
+        app.listen(port, () => console.log(okMessage));        
+    }
+)()
